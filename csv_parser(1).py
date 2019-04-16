6bbdc7168186295390f25980eef77e18cef50a88
NickWattsCS <46544773+NickWattsCS@users.noreply.github.com>
2019-04-15 22:36:51 -0400

    Added corrections
    Stores emails without newline as value in dictionary, added error checking

---
+++ csv_parser(1).py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+def csv_parser(name):
+	f = open(name, 'r')
+	employees = { }
+	for line in f:
+		print(line)
+		if line == "\n":
+			print("Empty line. Skipping.")
+			continue
+		line = line.split(',')
+		if len(line) < 2:
+			if line[0].find("@") != -1:
+				print("Line is missing name. Skipping.")
+			else:
+				print("Line is missing email. Skipping.")
+			continue
+		elif line[0].find("@") != -1:
+			print("Invalid syntax: first element is not a name.")
+			continue
+		else:
+			email = line[1][:-1]
+			employees[line[0]] = email
+	f.close()
+	return employees
