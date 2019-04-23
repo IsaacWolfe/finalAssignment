d0cb0eb5c7a8961552ad4bc5f127506695106287
NickWattsCS <46544773+NickWattsCS@users.noreply.github.com>
2019-04-23 07:59:53 -0400

    Correct csv_parser with Error Class

--- csv_parser.py
+++ csv_parser.py
@@ -1,27 +1,51 @@
 from __future__ import print_function
-import time
-import sys
 
-def csv_parser_file(name):
+class CSV_Error:
+	def __init__(self,l,d,e,n):
+		self.line = l
+		self.dependency = d
+		self.error = e
+		self.num = n
+
+	def __repr__(self):
+		if self.num == 0:
+			return "Line: " + str(self.line) + " Error: " + self.error
+		elif self.num == 1:
+			return "Line: " + str(self.line) + " Error: " + self.error + "\n\tFirst Element: " + self.dependency
+		elif self.num == 2:
+			return "Line: " + str(self.line) + " Error: " + self.error + "\n\tName: " + self.dependency
+		elif self.num == 3:
+			return "Line: " + str(self.line) + " Error: " + self.error + "\n\tEmail: " + self.dependency
+
+def csv_parser(name):
 	f = open(name, 'r')
 	employees = { }
+	errors = [ ]
+	line_count = 1
 	for line in f:
-		line.split(',')
-		if len(line) != 2:
-			print("Invalid number of arguments.")
-		elif line[1].find("@") != -1:
-			print("Invalid syntax: first element is not a name.")
+		if line == "\n":
+			print("Error found. Skipping line.")
+			errors.append(CSV_Error(line_count, "\n", "Empty line", 0))
+			continue
+		line = line.split(',')
+		if len(line) < 2:
+			if line[0].find("@") != -1:
+				print("Error found. Skipping line.")
+				errors.append(CSV_Error(line_count, line[0], "First element is not a name", 3))
+			else:
+				print("Error found. Skipping line.")
+				errors.append(CSV_Error(line_count, line[0], "Name does not have an associated email", 2))
+			continue
+		elif len(line) > 2:
+			print("Error found. Skipping line.")
+			errors.append(CSV_Error(line_count, line[0], "Line contains too many variables", 1))
+		elif line[0].find("@") != -1:
+			print("Error found. Skipping line.")
+			errors.append(CSV_Errors(line_count, line[0], "First element is not a name", 3))
 			continue
 		else:
-			person = [line[1], line[2]]
-			email = line[3]
-			employees[person] = email
+			email = line[1][:-1]
+			employees[line[0]] = email
 	f.close()
+	print_errors(errors);
 	return employees
-
-""" 
-def getDay():
-
-
-
-"""
